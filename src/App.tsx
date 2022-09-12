8b397b922dcead92d59c7a64cda2f0bd3e3fe413
Franz Noel Tanglao <franz@tanglao.org>
2022-09-11 18:49:23 -0700

    feat: Create a new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,85 @@
 import "./App.css";
 import * as React from 'react';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Typography } from "@mui/material";
-import { iTodo } from "@interfaces/iTodo";
-import { useQuery } from "react-query";
+import { Alert, Button, Snackbar, TextField, Typography } from "@mui/material";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 const columns: GridColDef[] = [
   // { field: 'id', headerName: 'ID', width: 300 },
   { field: 'description', headerName: 'Description', width: 700 },
 ];
 
-const initTodo = (todoRows: iTodo[]) => {
-  return todoRows;
-}
+const todoGet = async () => {
+  const res = await fetch(`${window.location.origin}/todo`);
+  return res.json();
+};
 
-const todoReducer = (todoState: iTodo[], todoAction: any) => {
-  switch(todoAction.type) {
-    case 'add':
-      return todoState;
-    default:
-      throw new Error();
-  }
-}
+const todoPost = async (newTodo: string) => {
+  const body = JSON.stringify({ description: newTodo });
+  const res = await fetch(`${window.location.origin}/todo`, { method: 'POST', body });
+  return res.json();
+};
 
 export default function App() {
-  const { isLoading, error, data: initialTodos } = useQuery('todos', async () => {
-    const res = await fetch(`${window.location.origin}/todo`);
-    return res.json();
+  const [newTodo, setNewTodo] = React.useState("");
+  const [isSnackbarOpen, setIsSnackbarOpen] = React.useState(true);
+  const queryClient = useQueryClient();
+
+  const todoGetQuery = useQuery('todos', todoGet);
+  const todoCreateMutation = useMutation(todoPost, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['todos']);
+      setIsSnackbarOpen(true);
+    }
   });
 
-  if (isLoading) {
+  if (todoGetQuery.isLoading || todoCreateMutation.isLoading) {
     return (
       <div>Loading...</div>
     )
   }
 
-  if (error) {
+  if (todoGetQuery.error) {
     return (
       <div>Error Loading</div>
     )
   }
 
-  console.log('todoState', initialTodos);
-  // const [todoState, dispatchTodo] = React.useReducer(todoReducer, initialTodos, initTodo);
+  const todoList = todoGetQuery.data;
+  const onNewTodoChange = (e: any) => setNewTodo(e.target.value);
+  const handleTodoSubmit = (e: any) => {
+    e.preventDefault();
+    if (newTodo) {
+      todoCreateMutation.mutate(newTodo);
+      setNewTodo("");
+    }
+  };
 
   return (
     <div style={{ height: 500, width: 700, margin: 10 }}>
       <Typography variant="h2">Todo</Typography>
+      <div style={{ padding: '10px 0'}}>
+        <form onSubmit={handleTodoSubmit}>
+          <TextField
+            id="new-todo-text"
+            onChange={onNewTodoChange}
+            label="Add Todo"
+            variant="outlined"
+            fullWidth
+            InputProps={{endAdornment: <Button type="submit" variant="contained">Add</Button>}}
+          />
+        </form>
+      </div>
+      <Snackbar
+        open={isSnackbarOpen}
+        autoHideDuration={2000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right'}}
+        onClose={() => setIsSnackbarOpen(false)}
+        >
+        <Alert severity="success">Successfully added or loaded Todo!</Alert>
+      </Snackbar>
       <DataGrid
-        rows={initialTodos}
+        rows={todoList}
         columns={columns}
         checkboxSelection
         hideFooter
